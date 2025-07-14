4b937337d39dab2bcb2c97608dab91e8e60ca378
HenderBerigo <henderson.berigo@gmail.com>
2025-07-14 09:50:15 -0300

    implantação da lógica do desafio

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 console.log("Desafio de Lógica 02 - DIO");
 console.log("Calculadora de Partidas Rankeadas");
+
+function calcularPartidasRankeadas(vitorias, derrotas){
+let saldoVitorias = vitorias - derrotas;
+
+let nivel;
+if(saldoVitorias < 10){
+  nivel = "Ferro"
+  } else if(saldoVitorias <= 20){
+  nivel = "Bronze"
+  } else if(saldoVitorias <= 50){
+  nivel = "Prata"
+  } else if(saldoVitorias <= 80){
+  nivel = "Ouro"
+  } else if(saldoVitorias <= 90){
+  nivel = "Diamante"
+  } else if(saldoVitorias <= 100){
+  nivel = "Lendário"
+  } else if(saldoVitorias >= 101){
+  nivel = "Imortal"
+  } else {
+  nivel = "Indefinido";
+}
+console.log(`O Herói tem de saldo de ${saldoVitorias} vitórias e está no nível de ${nivel}`);
+}
+
+calcularPartidasRankeadas(195, 12);
